6892c02870485278eb1919eebe54d5d3cf8becc7
MusicTsai <foreverlovekaggle3@gmail.com>
2023-04-25 04:03:49 +0800

    Update proofreader.py

--- addon/globalPlugins/GPTAssistant/lib/proofreader.py
+++ addon/globalPlugins/GPTAssistant/lib/proofreader.py
@@ -8,29 +8,32 @@ class Proofreader():
 	A class that provides a proofreading tool to refine texts.
 
 	Parameters:
-		segment_corrector (TypoCorrector): An instance of the TypoCorrector class that will be used to correct typos
-										   in the input text.
+		segment_corrector (TypoCorrector): An instance of the TypoCorrector for correcting typos in the input text.
 	"""
 
 	def __init__(self, segment_corrector: TypoCorrector):
 		self.segment_corrector = segment_corrector
 
-	def typo_analyzer(self, text: str, fake_operation: bool = False) -> Tuple:
+	def typo_analyzer(self, text: str, fake_corrected_text: str = None) -> Tuple:
 		"""
-		Analyzes the input text for typos and corrects them using the TypoCorrector instance passed to the Proofreader
+		Analyze the input text's typos and corrects them using the TypoCorrector instance passed to the Proofreader
 		constructor. It also calculates the difference between the original and corrected text.
 
 		Parameters:
 			text (str): The text to be analyzed for typos.
-			fake_operation (bool): If True, the function will not perform any corrections and will return the input text
-								   unchanged.
+			fake_corrected_text (str, optional): If specified, the function will return this text as the corrected
+												text instead of correcting the input text.
 
 		Returns:
 			A tuple containing the corrected text and a list of differences between the original and corrected text.
 		"""
+		if fake_corrected_text is not None:
+			return fake_corrected_text, strings_diff(text, fake_corrected_text)
+
 		text_corrected = ""
+
 		for segment in text_segmentation(text):
-			segment_corrected = self.segment_corrector.correct_string(segment, fake_operation)
+			segment_corrected = self.segment_corrector.correct_string(segment)
 			if segment_corrected is None:
 				text_corrected += ("■" * len(segment))
 				continue
