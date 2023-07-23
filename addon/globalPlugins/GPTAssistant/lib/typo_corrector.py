9ebb5f8eb8c5c2e321b1a586fc8692df3d1992d2
MusicTsai <foreverlovekaggle3@gmail.com>
2023-07-23 17:12:55 +0800

    Remove system part

--- addon/globalPlugins/GPTAssistant/lib/typo_corrector.py
+++ addon/globalPlugins/GPTAssistant/lib/typo_corrector.py
@@ -93,7 +93,6 @@ class BaseTypoCorrector():
 
 	def _chat_completion(self, prompt: str) -> str:
 		messages = [
-			{"role": "system", "content": "You are a helpful assistant."},
 			{"role": "user", "content": prompt},
 		]
 
