c66c76918dfb609f6d41ee31fbf59196aff6ee05
MusicTsai <foreverlovekaggle3@gmail.com>
2024-05-19 09:44:18 +0800

    Add codes for simplified Chinese

--- addon/globalPlugins/WordBridge/lib/template.py
+++ addon/globalPlugins/WordBridge/lib/template.py
@@ -1,11 +1,11 @@
-chinese_with_phone_templates = [
+with_phone_traditional_tw = [
 	{"role": "system", "content": "輸入為文字與其正確拼音，請修正錯字並輸出正確文字:\n(文字&拼音) => 文字"},
 	{"role": "user", "content": "(今天天器真好&jin1 tian1 tian1 qi4 zhen1 hao3) => "},
 	{"role": "assistant", "content": "今天天氣真好"},
 	{"role": "user", "content": "({{text_input}}&{{phone_input}}) => "},
 ]
 
-chinese_simple_templates = [
+simple_traditional_tw = [
 	{"role": "system", "content": "改錯字(避免加減字，或取代原讀音的字):"},
 	{"role": "user", "content": "天器真好 => "},
 	{"role": "assistant", "content": "天氣真好"},
@@ -14,14 +14,43 @@ chinese_simple_templates = [
 	{"role": "user", "content": "{{text_input}} => "},
 ]
 
-chinese_comment = "'{{response_previous}}'是錯誤答案，請修正重新輸出文字"
+with_phone_simplified = [
+	{"role": "system", "content": "输入为文字与其正确拼音，请修正错字并输出正确文字:\n(文字&拼音) => 文字"},
+	{"role": "user", "content": "(今天天器真好&jin1 tian1 tian1 qi4 zhen1 hao3) => "},
+	{"role": "assistant", "content": "今天天气真好"},
+	{"role": "user", "content": "({{text_input}}&{{phone_input}}) => "},
+]
+
+simple_simplified = [
+	{"role": "system", "content": "改错字(避免加减字，或取代原读音的字):"},
+	{"role": "user", "content": "天器真好 => "},
+	{"role": "assistant", "content": "天气真好"},
+	{"role": "user", "content": "出去玩 => "},
+	{"role": "assistant", "content": "出去玩"},
+	{"role": "user", "content": "{{text_input}} => "},
+]
+
+comment_traditional_tw = "'{{response_previous}}'是錯誤答案，請修正重新輸出文字"
+comment_simplified = "'{{response_previous}}'是错误答案，请修正重新输出文字"
 
 COMMENT_DICT = {
-	"ChineseTypoCorrectorSimple": chinese_comment,
-	"ChineseTypoCorrector": chinese_comment,
+	"ChineseTypoCorrectorSimple":{
+		"traditional_tw": comment_traditional_tw,
+		"simplified": comment_simplified,
+	},
+	"ChineseTypoCorrector":{
+		"traditional_tw": comment_traditional_tw,
+		"simplified": comment_simplified,
+	}
 }
 
 TEMPLATE_DICT = {
-	"ChineseTypoCorrectorSimple": chinese_simple_templates,
-	"ChineseTypoCorrector": chinese_with_phone_templates,
+	"ChineseTypoCorrectorSimple":{
+		"traditional_tw": simple_traditional_tw,
+		"simplified": simple_simplified,
+	},
+	"ChineseTypoCorrector":{
+		"traditional_tw": with_phone_traditional_tw,
+		"simplified": with_phone_simplified,
+	}
 }
--- addon/globalPlugins/WordBridge/lib/typo_corrector.py
+++ addon/globalPlugins/WordBridge/lib/typo_corrector.py
@@ -48,6 +48,7 @@ class BaseTypoCorrector():
 		max_correction_attempts: int = 3,
 		httppost_retries: int = 2,
 		backoff: int = 1,
+		output_format: str = "traditional_tw",
 	):
 
 		self.model = model
@@ -61,12 +62,13 @@ class BaseTypoCorrector():
 		self.httppost_retries = httppost_retries
 		self.backoff = backoff
 		self.headers = {"Authorization": f"Bearer {access_token}"}
+		self.output_format = output_format
 
 	def correct_segment(self, input_text: str, fake_operation: bool = False) -> str:
 		if fake_operation or not self._has_target_language(input_text):
 			return CorrectorResult(input_text, input_text, [])
 
-		template = deepcopy(TEMPLATE_DICT[self.__class__.__name__])
+		template = deepcopy(TEMPLATE_DICT[self.__class__.__name__][self.output_format])
 		text = self._text_preprocess(input_text)
 		input = self._create_input(template, text)
 
@@ -144,7 +146,7 @@ class BaseTypoCorrector():
 
 	def _chat_completion(self, input: List, response_text_history: List) -> str:
 		messages = deepcopy(input)
-		comment_template = COMMENT_DICT[self.__class__.__name__]
+		comment_template = COMMENT_DICT[self.__class__.__name__][self.output_format]
 		for response_previous in response_text_history:
 			comment = comment_template.replace("{{response_previous}}", response_previous)
 			messages.append({"role": "assistant", "content": response_previous})
