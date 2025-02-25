aa3b7d6b3592f7b5037ead41697ec45b7472b019
MusicTsai <foreverlovekaggle3@gmail.com>
2025-02-26 01:28:26 +0800

    Add Baidu's deepseek

--- addon/globalPlugins/WordBridge/dialogs.py
+++ addon/globalPlugins/WordBridge/dialogs.py
@@ -28,6 +28,7 @@ LABEL_DICT = {
 	"gpt-4o": _("gpt-4o"),
 	"gpt-4o-mini": _("gpt-4o-mini"),
 	"ernie-4.0-turbo-8k": _("ernie-4.0-turbo-8k"),
+	"deepseek-v3": _("deepseek-v3"),
 	"o1-mini": _("o1-mini"),
 	"o1-preview": _("o1-preview"),
 	"Standard Mode": _("Standard Mode"),
